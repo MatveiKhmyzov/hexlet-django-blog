c5bfd7e98db7330256f52e1b7d692f44f7b37f55
Matvei Khmyzov <matw.hmizov@yandex.ru>
2023-02-10 22:36:48 +0300

    Added pair class based views

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from hexlet_django_blog.views import IndexPage
 
 
-def index(request):
-    return render(
-        request,
-        'article/index.html',
-        context={'title': 'Articles'})
+class ArticleIndexPage(IndexPage):
+    template_name = 'article/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Articles'
+        return context
+
+
+# def index(request):
+#     return render(
+#         request,
+#         'article/index.html',
+#         context={'title': 'Articles'})
 
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
+class IndexPage(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'World'
+        return context
+
+
+# def index(request):
+#     return render(request, 'index.html', context={
+#         'who': 'World',
+#     })
 
 
 def about(request):

3f843ef4603448880502b4504f1b89878a0d72e9
Matvei Khmyzov <matw.hmizov@yandex.ru>
2023-02-15 10:41:46 +0300

    Configured work with secrets with dotenv

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()  # Загрузка переменных окружения из файла .env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f@bsl5&l__$1u7(r^%#&yqy#@nl6w^975slkk)cg1+$1u^1ygd'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
